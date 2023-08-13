5adc53fafab07ea33457fc331bb40168cb9a59c6
Nick J. Browning <nickjbrowning@gmail.com>
2023-08-13 22:35:05 +0200

    added some additional implementations

--- benchmark_cuda.py
+++ benchmark_cuda.py
@@ -6,10 +6,10 @@ import ops_cuda
 
 def benchmark(dtype, device):
 
-    nedges = 60000
+    nedges = 30000
     nnodes = 1000
-    nfeatures = 32
-    nl = 5
+    nfeatures = 96
+    nl = 16
 
     print(f"--DTYPE: {dtype}")
     print(f"Benchmarking dtype {dtype} and device {device}")
@@ -24,7 +24,7 @@ def benchmark(dtype, device):
     indices_cuda = indices.cuda().int()
 
     start = time.time()
-    for _ in range(100):
+    for _ in range(1000):
         neighbour_cuda = ops_cuda.calculate_neighbours(
             indices_cuda, nnodes, 64)
     finish = time.time()
@@ -32,20 +32,39 @@ def benchmark(dtype, device):
 
     X = a.cuda()
     Y = b.cuda()
-    start = time.time()
-    for _ in range(100):
+
+    #warmup
+    for _ in range(1000):
         output_cuda = ops_cuda.forward(X, Y, neighbour_cuda, nnodes,  32, 4, 1)
-    finish = time.time()
-    print(f"The CUDA implementation forward took {finish-start} seconds")
 
     start = time.time()
-    for _ in range(100):
+    for _ in range(1000):
+        output_cuda = ops_cuda.forward(X, Y, neighbour_cuda, nnodes,  32, 4, 1)
+    finish = time.time()
+    print(f"The CUDA implementation 1 forward took {finish-start} seconds")
+
+    start = time.time()
+    for _ in range(1000):
+        output_cuda = ops_cuda.forward2(X, Y, neighbour_cuda, nnodes,  32, 4, 1)
+    finish = time.time()
+    print(f"The CUDA implementation 2 forward took {finish-start} seconds")
+
+    start = time.time()
+    for _ in range(1000):
+        output_cuda = ops_cuda.forward3(X, Y, neighbour_cuda, nnodes,  32, 4, 1)
+    finish = time.time()
+    print(f"The CUDA implementation 3 forward took {finish-start} seconds")
+
+    print (output_cuda[0])
+
+    start = time.time()
+    for _ in range(1000):
         output_cpu = opt_ops(a, b, indices, nnodes)
     finish = time.time()
     print(f"The CPU implementation forward took {finish-start} seconds")
 
     start = time.time()
-    for _ in range(100):
+    for _ in range(1000):
         loss = torch.sum(opt_ops(a, b, indices, nnodes))
         loss.backward()
     finish = time.time()
--- ops/ops.cu
+++ ops/ops.cu
@@ -14,7 +14,7 @@ using namespace torch;
 
 #define FULL_MASK 0xffffffff
 
-__host__ __device__ int find_integer_divisor(int x, int bdim)
+__host__ __device__ int32_t find_integer_divisor(int32_t x, int32_t bdim)
 {
     return (x + bdim - 1) / bdim;
 }
@@ -29,7 +29,6 @@ __global__ void forward_kernel(
     extern __shared__ char buffer[];
     size_t offset = 0;
     scalar_t *smem = reinterpret_cast<scalar_t *>(buffer + offset);
-    offset += blockDim.x * sizeof(scalar_t);
 
     int32_t edge_start = indices[blockIdx.x];
     int32_t edge_end = 0;
@@ -46,9 +45,9 @@ __global__ void forward_kernel(
     // clear out shared memory storage...
     for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
     {
-        for (int32_t channel = threadIdx.x; channel < X.size(1); channel += blockDim.x)
+        for (int32_t x_id = threadIdx.x; x_id < X.size(1); x_id += blockDim.x)
         {
-            smem[m * X.size(1) + channel] = 0.0;
+            smem[m * X.size(1) + x_id] = 0.0;
         }
     }
 
@@ -60,11 +59,11 @@ __global__ void forward_kernel(
         {
             scalar_t y = Y[i][m];
 
-            for (int32_t channel = threadIdx.x; channel < X.size(1); channel += blockDim.x)
+            for (int32_t x_id = threadIdx.x; x_id < X.size(1); x_id += blockDim.x)
             {
-                scalar_t x = X[i][channel];
+                scalar_t x = X[i][x_id];
 
-                smem[m * X.size(1) + channel] += x * y;
+                smem[m * X.size(1) + x_id] += x * y;
             }
         }
     }
@@ -73,9 +72,9 @@ __global__ void forward_kernel(
 
     for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
     {
-        for (int32_t channel = threadIdx.x; channel < X.size(1); channel += blockDim.x)
+        for (int32_t x_id = threadIdx.x; x_id < X.size(1); x_id += blockDim.x)
         {
-            output[blockIdx.x][m][channel] = smem[m * X.size(1) + channel];
+            output[blockIdx.x][m][x_id] = smem[m * X.size(1) + x_id];
         }
     }
 }
@@ -116,6 +115,189 @@ torch::Tensor forward_gpu(torch::Tensor X,
     return output;
 }
 
+template <typename scalar_t>
+__global__ void forward2_kernel(
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> X,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> Y,
+    const torch::PackedTensorAccessor32<int32_t, 1, torch::RestrictPtrTraits> indices,
+    torch::PackedTensorAccessor32<scalar_t, 3, torch::RestrictPtrTraits> output)
+{
+    extern __shared__ char buffer[];
+    size_t offset = 0;
+    scalar_t *smem = reinterpret_cast<scalar_t *>(buffer + offset);
+
+    int32_t edge_start = indices[blockIdx.x];
+    int32_t edge_end = 0;
+
+    if (blockIdx.x == indices.size(0) - 1)
+    {
+        edge_end = Y.size(0) - 1;
+    }
+    else
+    {
+        edge_end = indices[blockIdx.x + 1];
+    }
+
+    int32_t feat_start = blockIdx.y * blockDim.x;
+
+    // clear out shared memory storage...
+    for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
+    {
+        smem[m * blockDim.x + threadIdx.x] = 0.0;
+    }
+
+    __syncthreads();
+
+    for (int32_t i = edge_start; i < edge_end; i++)
+    {
+        for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
+        {
+            scalar_t y = Y[i][m];
+
+            scalar_t x = 0.0;
+
+            if (feat_start + threadIdx.x < X.size(1))
+            {
+                x = X[i][feat_start + threadIdx.x];
+            }
+
+            smem[m * blockDim.x + threadIdx.x] += x * y;
+        }
+    }
+
+    __syncthreads();
+
+    for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
+    {
+        if (feat_start + threadIdx.x < X.size(1))
+        {
+            output[blockIdx.x][m][feat_start + threadIdx.x] = smem[m * blockDim.x + threadIdx.x];
+        }
+    }
+}
+
+torch::Tensor forward2_gpu(torch::Tensor X,
+                           torch::Tensor Y,
+                           torch::Tensor neighbour_indices,
+                           int32_t natoms,
+                           int32_t nthreadx,
+                           int32_t nthready,
+                           int32_t nthreadz)
+{
+
+    torch::Tensor output = torch::empty({natoms, Y.size(1), X.size(1)},
+                                        torch::TensorOptions()
+                                            .dtype(X.dtype())
+                                            .device(X.device()));
+
+    int32_t nby = find_integer_divisor(X.size(1), nthreadx);
+
+    dim3 block_dim(natoms, nby);
+
+    dim3 grid_dim(nthreadx, nthready, 1);
+
+    AT_DISPATCH_FLOATING_TYPES(
+        X.type(), "forward2_gpu", ([&]
+                                   {
+                    size_t total_buff_size = 0;
+
+                    total_buff_size += nthreadx * Y.size(1) * sizeof(scalar_t);
+
+                    forward2_kernel<scalar_t><<<block_dim, grid_dim, total_buff_size>>>(
+                        X.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                        Y.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                        neighbour_indices.packed_accessor32<int32_t, 1, torch::RestrictPtrTraits>(),
+                        output.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()); }));
+
+    cudaDeviceSynchronize();
+
+    return output;
+}
+
+template <typename scalar_t>
+__global__ void forward3_kernel(
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> X,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> Y,
+    const torch::PackedTensorAccessor32<int32_t, 1, torch::RestrictPtrTraits> indices,
+    torch::PackedTensorAccessor32<scalar_t, 3, torch::RestrictPtrTraits> output)
+{
+
+    int32_t edge_start = indices[blockIdx.x];
+    int32_t edge_end = 0;
+
+    if (blockIdx.x == indices.size(0) - 1)
+    {
+        edge_end = Y.size(0) - 1;
+    }
+    else
+    {
+        edge_end = indices[blockIdx.x + 1];
+    }
+
+    int32_t feat_start = blockIdx.y * blockDim.x;
+
+    __syncthreads();
+
+    for (int32_t m = threadIdx.y; m < Y.size(1); m += blockDim.y)
+    {
+        scalar_t tmp_output = 0.0;
+
+        for (int32_t i = edge_start; i < edge_end; i++)
+        {
+
+            scalar_t y = Y[i][m];
+            scalar_t x = 0.0;
+
+            if (feat_start + threadIdx.x < X.size(1))
+            {
+                x = X[i][feat_start + threadIdx.x];
+            }
+
+            tmp_output += x * y;
+        }
+
+        output[blockIdx.x][m][feat_start + threadIdx.x] = tmp_output;
+    }
+}
+
+torch::Tensor forward3_gpu(torch::Tensor X,
+                           torch::Tensor Y,
+                           torch::Tensor neighbour_indices,
+                           int32_t natoms,
+                           int32_t nthreadx,
+                           int32_t nthready,
+                           int32_t nthreadz)
+{
+
+    torch::Tensor output = torch::empty({natoms, Y.size(1), X.size(1)},
+                                        torch::TensorOptions()
+                                            .dtype(X.dtype())
+                                            .device(X.device()));
+
+    int32_t nby = find_integer_divisor(X.size(1), nthreadx);
+
+    dim3 block_dim(natoms, nby);
+
+    dim3 grid_dim(nthreadx, nthready, 1);
+
+    AT_DISPATCH_FLOATING_TYPES(
+        X.type(), "forward3_gpu", ([&]
+                                   {
+                    size_t total_buff_size = 0;
+
+                    //total_buff_size += nthreadx * Y.size(1) * sizeof(scalar_t);
+
+                    forward3_kernel<scalar_t><<<block_dim, grid_dim, total_buff_size>>>(
+                        X.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                        Y.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                        neighbour_indices.packed_accessor32<int32_t, 1, torch::RestrictPtrTraits>(),
+                        output.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()); }));
+
+    cudaDeviceSynchronize();
+
+    return output;
+}
+
 #define NEIGHBOUR_NEDGES_PER_BLOCK 512
 
 /*
@@ -138,7 +320,7 @@ __global__ void calculate_neighbours_kernel(const torch::PackedTensorAccessor32<
     int32_t nedges = sender_list.size(0);
 
     // load all elements of senderlist needed by block into shared memory
-    for (int i = threadIdx.x; i < NEIGHBOUR_NEDGES_PER_BLOCK + 1; i += blockDim.x)
+    for (int32_t i = threadIdx.x; i < NEIGHBOUR_NEDGES_PER_BLOCK + 1; i += blockDim.x)
     {
         int32_t idx = block_start + i;
 
@@ -151,7 +333,7 @@ __global__ void calculate_neighbours_kernel(const torch::PackedTensorAccessor32<
     __syncthreads();
 
     // deal with even boundaries
-    for (int i = 2 * threadIdx.x; i < NEIGHBOUR_NEDGES_PER_BLOCK; i += 2 * blockDim.x)
+    for (int32_t i = 2 * threadIdx.x; i < NEIGHBOUR_NEDGES_PER_BLOCK; i += 2 * blockDim.x)
     {
         int32_t idx = block_start + i;
 
@@ -168,7 +350,7 @@ __global__ void calculate_neighbours_kernel(const torch::PackedTensorAccessor32<
     }
 
     // deal with odd boundaries
-    for (int i = 2 * threadIdx.x + 1; i < NEIGHBOUR_NEDGES_PER_BLOCK + 1; i += 2 * blockDim.x)
+    for (int32_t i = 2 * threadIdx.x + 1; i < NEIGHBOUR_NEDGES_PER_BLOCK + 1; i += 2 * blockDim.x)
     {
         int32_t idx = block_start + i;
 
@@ -199,11 +381,9 @@ torch::Tensor calculate_neighbours_gpu(torch::Tensor sender_list, int32_t natoms
                                                     .dtype(sender_list.dtype())
                                                     .device(sender_list.device()));
 
-    int nbx = find_integer_divisor(sender_list.size(0), NEIGHBOUR_NEDGES_PER_BLOCK);
+    int32_t nbx = find_integer_divisor(sender_list.size(0), NEIGHBOUR_NEDGES_PER_BLOCK);
 
     dim3 block_dim(nbx);
-
-    // printf("block dim: %d\n", nbx);
 
     dim3 grid_dim(nthreadx, 1, 1);
 
@@ -225,4 +405,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     m.def("calculate_neighbours", &calculate_neighbours_gpu, "computes neighbourlist starts from sender list.");
     m.def("forward", &forward_gpu, "ops forward GPU.");
+    m.def("forward2", &forward2_gpu, "ops forward 2 GPU.");
+    m.def("forward3", &forward3_gpu, "ops forward 2 GPU.");
 }
