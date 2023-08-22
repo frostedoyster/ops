14e22ff21a76cbfe1d8cde07ef8ff1f1cd5c2da8
Nick J. Browning <nickjbrowning@gmail.com>
2023-08-22 09:47:43 +0200

    testing.

--- ops/lib/ops.py
+++ ops/lib/ops.py
@@ -3,6 +3,41 @@ import sysconfig
 import torch
 
 
+import os
+import sysconfig
+import torch
+
+_HERE = os.path.realpath(os.path.dirname(__file__))
+EXT_SUFFIX = sysconfig.get_config_var('EXT_SUFFIX')
+
+torch.ops.load_library(_HERE + '/ops_cc.so')
+if torch.cuda.is_available():
+    torch.ops.load_library(_HERE + '/ops_cuda.so')
+
+'''
+    differenciable outer product
+'''
+
+def outer_product(X, Y, sender_list, nnodes):
+    return torch.ops.ops.outer_product(X, Y, sender_list, nnodes)
+
+'''
+    Non-differenciable - for testing only.
+'''
+
+
+def forward(X, Y, sender_list, nnodes, nthreadx=32, nthready=4, nthreadz=1):
+    return torch.ops.ops.forward(X, Y, sender_list, nnodes, nthreadx, nthready, nthreadz)
+
+
+def backward(X, Y, grad_in, neighbours, nnodes):
+    return torch.ops.ops.backward(X, Y, grad_in, neighbours, nnodes)
+
+
+def calculate_neighbours(sender_list, nnodes, nthreadx=64):
+    return torch.ops.ops.calculate_neighbours(sender_list, nnodes, nthreadx)
+
+
 def ref_ops(tensor_a, tensor_b, scatter_indices, out_dim):
     # Reference implementation
 
@@ -20,13 +55,6 @@ def ref_ops(tensor_a, tensor_b, scatter_indices, out_dim):
     )
     return result
 
-
-_HERE = os.path.realpath(os.path.dirname(__file__))
-EXT_SUFFIX = sysconfig.get_config_var('EXT_SUFFIX')
-
-torch.ops.load_library(_HERE + '/ops_cc.so')
-if torch.cuda.is_available():
-    torch.ops.load_library(_HERE + '/ops_cuda.so')
 
 def opt_ops(tensor_a, tensor_b, scatter_indices, out_dim):
 
